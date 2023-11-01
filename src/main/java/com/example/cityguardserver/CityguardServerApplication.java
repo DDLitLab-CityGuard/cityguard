4fb16f5c81c346f7f8bd6114f5b5f202e092e7c9
MarvinMielchen <mielchen.2001@gmail.com>
2023-11-01 10:09:18 +0100

    init

---
+++ src/main/java/com/example/cityguardserver/CityguardServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.cityguardserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityguardServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityguardServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cityguardserver/CityguardServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cityguardserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityguardServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
