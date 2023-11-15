1f0337268e12cc08e12a8e39dda84ffb3dce1224
Marvin Mielchen <100961250+marvinmielchen@users.noreply.github.com>
2023-11-15 11:32:37 +0100

    First heatmap implementation (#8)
    * fix spacing in main.css
    
    * update
    
    * update
    
    * first working heatmap visualization
    
    * finished basic heatmap implementation

--- mapdisplay.js
+++ mapdisplay.js
@@ -12,18 +12,36 @@ map.on('moveend', function(ev) {
 });
 
 
+const heatmapGroup = L.layerGroup().addTo(map);
 const markerGroup = L.layerGroup().addTo(map);
 
 
+
 function fetchandrender() {
-    fetch('http://127.0.0.1:5123/api/fetchreports')
+    latl = map.getBounds().getSouthWest().lat - 0.01
+    lonl = map.getBounds().getSouthWest().lng -0.01
+    latu = map.getBounds().getNorthEast().lat + 0.01
+    lonu = map.getBounds().getNorthEast().lng + 0.01
+    fetch(`http://127.0.0.1:5123/api/fetchreports?latitudeLower=${latl}&latitudeUpper=${latu}&longitudeLeft=${lonl}&longitudeRight=${lonu}`)
         .then(response => response.json())
         .then(data => {
-            console.log(data)
+            heatmapGroup.clearLayers();
             markerGroup.clearLayers();
-
-            for (var i = 0; i < data.length; i++) {
-                marker = L.marker([data[i].latitude, data[i].longitude]).addTo(markerGroup);
+            for (var i = 0; i < data.markers.length; i++) {
+                L.marker([data.markers[i].latitude, data.markers[i].longitude]).addTo(markerGroup);
+            }
+            for(var i = 0; i < data.heatmap.length; i++){
+                const x = data.heatmap[i].latitude;
+                const y = data.heatmap[i].longitude;
+                const sizeLat = data.heatmap[i].sizeLat;
+                const sizeLon = data.heatmap[i].sizeLon;
+                var latlngs = [
+                    [x, y],
+                    [x + (sizeLat), y],
+                    [x+ (sizeLat), y + sizeLon],
+                    [x, y + sizeLon],
+                ];
+                L.polygon(latlngs, {color: '#ff0000', weight: 0, fillOpacity: data.heatmap[i].value}).addTo(heatmapGroup);
             }
         });
 }
