a0c44a4e4a351bf4eb14ec9a1ee52d89dc35b490
Marvin Mielchen <100961250+marvinmielchen@users.noreply.github.com>
2023-11-17 18:52:27 +0100

    add current location functionality (#18)

--- submitevent.js
+++ submitevent.js
@@ -82,5 +82,32 @@ function makePostRequest(){
   });
 
 
+const checkbox = document.getElementById("locationcheckbox");
+const eingabeFeld = document.getElementById("location");
 
+checkbox.addEventListener("change", async function() {
+  if (checkbox.checked) {
+    eingabeFeld.disabled = true;
+    eingabeFeld.classList.add("text-muted");
+    const coordinates = await getCoordinates(); 
+    eingabeFeld.value = `${coordinates.latitude},${coordinates.longitude}`;
+  } else {
+    eingabeFeld.disabled = false;
+    eingabeFeld.classList.remove("text-muted");
+  }
+});
 
+function getCoordinates() {
+  if (navigator.geolocation) {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        resolve({ latitude, longitude });
+      });
+    });
+  } else {
+    alert("Geolokalisierung wird in diesem Browser nicht unterstützt.");
+    return null;
+  }
+}
