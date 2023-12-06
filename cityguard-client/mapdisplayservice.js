3d752403ff1630b72e0b88972f3fc2d3f7c50100
0mielche <marvin.mielchen@studium.uni-hamburg.de>
2023-12-06 16:19:23 +0100

    client side source code documentation

--- cityguard-client/mapdisplayservice.js
+++ cityguard-client/mapdisplayservice.js
@@ -1,5 +1,18 @@
+/**
+ * This module is responsible for the display of the map together with the collected heatmap and markers from the api.
+ * It delegates the fetching of the data to the api module and renders the data on the map.
+ *
+ * @module mapdisplayservice
+ */
 import {fetchReports} from "./api/cityguard-api.js";
 
+/**
+ * This function collects the data from the api and renders it on the map by putting it into the heatmapGroup and markerGroup.
+ *
+ * @param map
+ * @param heatmapGroup
+ * @param markerGroup
+ */
 export function fetchAndRenderReports(map, heatmapGroup, markerGroup){
 	fetchReports(
 		map.getBounds().getSouthWest().lat - 0.01,
