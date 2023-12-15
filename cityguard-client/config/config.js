6c55e74a945fbf0a45e7e3b8d861528672ec0daf
Duru Kececi <138002775+durukececi@users.noreply.github.com>
2023-12-15 14:48:07 +0100

    5 geocoding api (#23)

--- cityguard-client/config/config.js
+++ cityguard-client/config/config.js
@@ -6,4 +6,7 @@
  * The Base URL of the CityGuard API.
  * @type {string}
  */
+
 export const apiEndpoint = `http://localhost:8088/api`
+export const geoCoderApiEndpoint =`https://cityguard.isa.uni-hamburg.de/nominatim/search?q=`
+export const geoCoderApiEndpointReverse =`https://cityguard.isa.uni-hamburg.de/nominatim/reverse?format=json&`
