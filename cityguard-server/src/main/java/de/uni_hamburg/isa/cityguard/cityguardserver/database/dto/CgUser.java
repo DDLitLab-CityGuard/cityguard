cbde738a373629f5c658e7a70d893ea35b7537f3
zeynepkececi@live.com <zeynepkececi@live.com>
2024-03-06 15:18:10 +0100

    user unique test and delete username

--- cityguard-server/src/main/java/de/uni_hamburg/isa/cityguard/cityguardserver/database/dto/CgUser.java
+++ cityguard-server/src/main/java/de/uni_hamburg/isa/cityguard/cityguardserver/database/dto/CgUser.java
@@ -12,7 +12,7 @@ public class CgUser {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	private String username;
+	@Column(unique = true)
 	private String email;
 	private String password;
 	private String firstname;
