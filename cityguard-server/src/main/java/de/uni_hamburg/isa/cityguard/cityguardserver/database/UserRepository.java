5a07e9b961a15e0e664fc5f52f022de558c36650
zeynepkececi@live.com <zeynepkececi@live.com>
2024-03-06 15:37:55 +0100

    hot fix

--- cityguard-server/src/main/java/de/uni_hamburg/isa/cityguard/cityguardserver/database/UserRepository.java
+++ cityguard-server/src/main/java/de/uni_hamburg/isa/cityguard/cityguardserver/database/UserRepository.java
@@ -2,13 +2,12 @@ package de.uni_hamburg.isa.cityguard.cityguardserver.database;
 
 import de.uni_hamburg.isa.cityguard.cityguardserver.database.dto.CgUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<CgUser, Long> {
 	Optional<CgUser> findByEmail(String email);
-	Optional<CgUser> findByUsernameOrEmail(String username, String email);
-	Optional<CgUser> findByUsername(String username);
-	Boolean existsByUsername(String username);
 	Boolean existsByEmail(String email);
 }
