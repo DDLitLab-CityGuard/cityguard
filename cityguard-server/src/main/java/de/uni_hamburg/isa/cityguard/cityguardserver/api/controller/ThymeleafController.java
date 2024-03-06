c85969377a0045cd3218cc036ace382d88a14ce8
0mielche <marvin.mielchen@studium.uni-hamburg.de>
2024-03-06 13:41:23 +0100

    update

--- cityguard-server/src/main/java/de/uni_hamburg/isa/cityguard/cityguardserver/api/controller/ThymeleafController.java
+++ cityguard-server/src/main/java/de/uni_hamburg/isa/cityguard/cityguardserver/api/controller/ThymeleafController.java
@@ -1,19 +1,40 @@
 package de.uni_hamburg.isa.cityguard.cityguardserver.api.controller;
 
+import de.uni_hamburg.isa.cityguard.cityguardserver.api.dto.UserLogin;
+import de.uni_hamburg.isa.cityguard.cityguardserver.api.dto.UserRegisterForm;
+import de.uni_hamburg.isa.cityguard.cityguardserver.database.AuthenticationRepository;
+import de.uni_hamburg.isa.cityguard.cityguardserver.database.UserRepository;
+import de.uni_hamburg.isa.cityguard.cityguardserver.database.dto.AuthenticationToken;
+import de.uni_hamburg.isa.cityguard.cityguardserver.database.dto.CgUser;
 import jakarta.servlet.http.HttpSession;
+import lombok.AllArgsConstructor;
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.Random;
 
 @Controller
+@AllArgsConstructor
 public class ThymeleafController {
-	@GetMapping("/welcome")
-	public String hello(Model model) {
 
+	private final UserRepository userRepository;
+	private final AuthenticationRepository authenticationRepository;
+	private final Random random = new Random();
+
+	@GetMapping("/welcome")
+	public String hello() {
 		return "welcome";
 	}
 
-@GetMapping("/index")
+	@GetMapping("/index")
 	public String index(HttpSession session) {
 		if (session.getAttribute("token") != null) {
 			System.out.println("Token: " + session.getAttribute("token"));
@@ -21,12 +42,51 @@ public class ThymeleafController {
 		return "index";
 	}
 
-
-
 	@GetMapping("/login")
 	public String login() {
 
 		return "login";
 	}
+
+	@PostMapping("/login")
+	public String authenticateUser(@ModelAttribute UserLogin loginDto, HttpSession session) {
+		String email = loginDto.getUsernameOrEmail();
+		System.out.println("email: " + email);
+		String password = loginDto.getPassword();
+		Optional<CgUser> user = userRepository.findByEmail(email);
+		for (CgUser cgUser : userRepository.findAll()) {
+			System.out.println(cgUser.getEmail() + " " + cgUser.getPassword());
+		}
+
+		if (user.isPresent()) {
+			if (BCrypt.checkpw(password, user.get().getPassword())) {
+				//create token
+				AuthenticationToken token = new AuthenticationToken();
+				token.setId(random.nextLong());
+				LocalDateTime expirationDate = LocalDateTime.now().plusDays(1);
+				token.setExpirationDate(expirationDate);
+				token.setCgUser(user.get());
+				authenticationRepository.save(token);
+				session.setAttribute("token", token.getId());
+				return "redirect:/index";
+			}
+		}
+		return "/login";
+	}
+
+	@PostMapping("/welcome")
+	public ResponseEntity<String> registerUser(@ModelAttribute UserRegisterForm registerDto){
+		System.out.println("registering user");
+		CgUser cgUser =new CgUser();
+		String encodedPassword = BCrypt.hashpw(registerDto.getPassword(), BCrypt.gensalt(6));
+		cgUser.setPassword(encodedPassword);
+		cgUser.setUsername(registerDto.getUsername());
+		cgUser.setFirstname(registerDto.getFirstname());
+		cgUser.setLastname(registerDto.getLastname());
+		cgUser.setEmail(registerDto.getEmail());
+		userRepository.save(cgUser);
+		return new ResponseEntity<>("User created successfully!.", HttpStatus.OK);
+	}
+
 
 }
